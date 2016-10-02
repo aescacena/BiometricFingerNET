9bd83fe21269623a221cebc8fd283350db0256ff
aescacena <aescacena@gmail.com>
2016-10-02 18:17:03 +0200

    Se ha puesto el campo user_id como autoincrementable en BBDD por que generaba una excepción de clave primaria, ahora realiza las insercciones de usuarios correctamente

--- BiometricFinger/Program.cs
+++ BiometricFinger/Program.cs
@@ -1,11 +1,13 @@
 ﻿using SourceAFIS.Simple;
 using System;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
 
 namespace BiometricFinger
 {
@@ -21,7 +23,7 @@ namespace BiometricFinger
             {
                 insertaHuellasDesdeCarpeta();
                 var usuarios = context.Usuario.ToList();
-                foreach(var usu in usuarios)
+                foreach (var usu in usuarios)
                 {
                     byte[] finger = usu.finger;
                     MemoryStream stream = new MemoryStream(finger, 0, finger.Length);
@@ -30,31 +32,37 @@ namespace BiometricFinger
                     Console.WriteLine(usu.id + " " + usu.username + " " + usu.finger);
                 }
             }
-//            Application.EnableVisualStyles();
-  //          Application.SetCompatibleTextRenderingDefault(false);
-    //        Application.Run(new Form1());
+            //            Application.EnableVisualStyles();
+            //          Application.SetCompatibleTextRenderingDefault(false);
+            //        Application.Run(new Form1());
         }
         static void insertaHuellasDesdeCarpeta()
         {
             // Initialize path to images
-           DirectoryInfo di = new DirectoryInfo(@"C:\Users\PC_STE_19\Documents\visual studio 2015\Projects\BiometricFinger\images");
-           Console.WriteLine("No search pattern returns:");
+            //DirectoryInfo di = new DirectoryInfo(@"C:\Users\PC_STE_19\Documents\visual studio 2015\Projects\BiometricFinger\images");
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\aesca\OneDrive\Documentos\Visual Studio 2015\Projects\BiometricFinger\images");
+            Console.WriteLine("No search pattern returns:");
+            List<Usuario> usuarios = new List<Usuario>();
             using (var context = new db_Entidades())
             {
-                int count = 0;
                 foreach (var fi in di.GetFiles())
                 {
-                    count++;
-                    Bitmap image1 = (Bitmap)Image.FromFile(@"C:\Users\PC_STE_19\Documents\Visual Studio 2015\Projects\BiometricFinger\images\" + fi.Name, true);
+                    AfisEngine Afis = new AfisEngine();
+                    //Bitmap image1 = (Bitmap)Image.FromFile(@"C:\Users\PC_STE_19\Documents\Visual Studio 2015\Projects\BiometricFinger\images\" + fi.Name, true);
+                    Bitmap image1 = (Bitmap)Image.FromFile(@"C:\Users\aesca\OneDrive\Documentos\Visual Studio 2015\Projects\BiometricFinger\images\" + fi.Name, true);
                     Fingerprint f = new Fingerprint();
                     f.AsBitmap = image1;
                     Usuario usu = new Usuario();
-                    usu.id = count;
+                    Person persona = new Person();
+                    persona.Fingerprints.Add(f);
+                    Afis.Extract(persona);
                     usu.username = fi.Name;
-                    usu.finger = new byte[100];
-                    context.Usuario.Add(usu);
+                    usu.finger = f.AsIsoTemplate;
+                    usuarios.Add(usu);
                     Console.WriteLine(fi.Name);
+                    context.Usuario.Add(usu);
                 }
+                context.SaveChanges();
             }
         }
     }
